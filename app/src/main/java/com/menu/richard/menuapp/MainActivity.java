6b38fb7a4981dd1a8c674eb454a66429704bc768
richard <rkromoser@gmail.com>
2018-07-13 11:43:37 +0200

    added Navigation menu

--- app/src/main/java/com/menu/richard/menuapp/MainActivity.java
+++ app/src/main/java/com/menu/richard/menuapp/MainActivity.java
@@ -1,39 +1,64 @@
 package com.menu.richard.menuapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import com.menu.richard.menuapp.DBHandler.DatabaseAccess;
 import com.menu.richard.menuapp.Entities.Unit;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private GridView gridView;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+    //private GridView gridView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeButtonEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        /*     super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         this.gridView = (GridView) findViewById(R.id.gridview);
         DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
         databaseAccess.open();
         List<Unit> units = databaseAccess.getUnits();
-        System.out.println("MAIN");
         databaseAccess.close();
 
         ArrayAdapter<Unit> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, units);
         this.gridView.setAdapter(adapter);
-
+ */
     }
 
     @Override
@@ -43,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
+ /*   @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -57,4 +82,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+*/
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_meal:
+                Toast.makeText(this, "Clicked Essensplan", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_recipe:
+                Toast.makeText(this, "Clicked Rezept Index", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_ingredient:
+                Toast.makeText(this, "Clicked Zutatenliste", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Clicked Senden", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Clicked Teilen", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
 }

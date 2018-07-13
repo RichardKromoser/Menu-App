b32b5e23ff8177f9e254fadd4763014a395eabb0
richard <rkromoser@gmail.com>
2018-07-13 20:04:56 +0200

    removed triple dots at top right

--- app/src/main/java/com/menu/richard/menuapp/MainActivity.java
+++ app/src/main/java/com/menu/richard/menuapp/MainActivity.java
@@ -43,64 +43,6 @@ public class MainActivity extends AppCompatActivity
         //add this line to display menu1 when the activity is loaded
         displaySelectedScreen(R.id.nav_ingredient);
     }
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeButtonEnabled(true);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-*/
-        /*     super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        this.gridView = (GridView) findViewById(R.id.gridview);
-        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
-        databaseAccess.open();
-        List<Unit> units = databaseAccess.getUnits();
-        databaseAccess.close();
-
-        ArrayAdapter<Unit> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, units);
-        this.gridView.setAdapter(adapter);
- */
-    //}
-
-  /*  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-       // if (id == R.id.action_settings) {
-         //   return true;
-        //}
-
-        return super.onOptionsItemSelected(item);
-    }
-*/
 
     @Override
     public void onBackPressed() {
@@ -115,7 +57,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_navigation_drawer, menu);
+        //adds the 3 dots on top right
+      //  getMenuInflater().inflate(R.menu.activity_navigation_drawer, menu);
         return true;
     }
 

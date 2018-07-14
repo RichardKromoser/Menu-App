5ad9999f14bd0dd8a286cdbec637d904ba429b95
remmi21 <rimmi21@hotmail.com>
2018-07-14 21:17:10 +0200

    added buttons to detailed view

--- app/src/main/java/com/menu/richard/menuapp/DetailedMeal.java
+++ app/src/main/java/com/menu/richard/menuapp/DetailedMeal.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridView;
 
-public class DetailedMeal extends Fragment {
+public class DetailedMeal extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.detailed_meal,container,false);
+        Button bIngredient = (Button) view.findViewById(R.id.buttonIngredient);
+        bIngredient.setOnClickListener(this);
+        Button bInstruction = (Button) view.findViewById(R.id.buttonInstruction);
+        bInstruction.setOnClickListener(this);
         return view;
     }
 
@@ -21,5 +26,17 @@ public class DetailedMeal extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Detailed Meal");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonIngredient:
+                System.out.println("Ingredient");
+                break;
+            case R.id.buttonInstruction:
+                System.out.println("Instruction");
+                break;
+        }
     }
 }

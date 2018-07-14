7b1f08ae2ce1aa1028e3aba38959a7d667694c70
remmi21 <rimmi21@hotmail.com>
2018-07-14 16:28:32 +0200

    comments for grid in image view

--- app/src/main/java/com/menu/richard/menuapp/GridViewAdapter.java
+++ app/src/main/java/com/menu/richard/menuapp/GridViewAdapter.java
@@ -1,6 +1,9 @@
 package com.menu.richard.menuapp;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -8,6 +11,10 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.menu.richard.menuapp.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class GridViewAdapter extends BaseAdapter {
     private Context mContext;
@@ -30,6 +37,7 @@ public class GridViewAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
+        List<String> list_img;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
@@ -38,6 +46,17 @@ public class GridViewAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
+
+       /* try {
+            InputStream ims = getAssets().open(list_img.get(i));
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            // set image to ImageView
+            imageView.setImageDrawable(d);
+        }
+        catch(IOException ex) {
+            return;
+        }*/
         imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }

a80366ac531c8d45c4b4f4598c68d2e55f28892c
remmi21 <rimmi21@hotmail.com>
2018-07-14 21:02:25 +0200

    added listener to grid

--- app/src/main/java/com/menu/richard/menuapp/RecipeIndex.java
+++ app/src/main/java/com/menu/richard/menuapp/RecipeIndex.java
@@ -6,7 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class RecipeIndex extends Fragment {
 
@@ -14,6 +16,16 @@ public class RecipeIndex extends Fragment {
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recipe_index,container,false);
         GridView gridView = (GridView) view.findViewById(R.id.gridviewRecipe);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                String a = String.valueOf(position);
+                System.out.println(parent.getItemAtPosition(position));
+                Toast.makeText(getContext().getApplicationContext(), a, Toast.LENGTH_SHORT).show();
+            }
+        });
         gridView.setAdapter(new GridViewAdapter(view.getContext())); // uses the view to get the context instead of getActivity().
         return view;
     }
